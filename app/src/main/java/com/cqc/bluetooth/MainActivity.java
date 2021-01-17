489271a373d33ab1a8d869148a85e0df19315a3f
CQC <1581908690@qq.com>
2021-01-17 13:25:10 +0800

    finished： isSupportBlueTooth getBlueToothStatus turnOnBlueTooth turnOffBlueTooth

--- app/src/main/java/com/cqc/bluetooth/MainActivity.java
+++ app/src/main/java/com/cqc/bluetooth/MainActivity.java
@@ -1,7 +1,13 @@
 package com.cqc.bluetooth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -11,10 +17,37 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 0;
     private BlueToothController mController = new BlueToothController();
     private Toast mToast;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,-1);
+            switch (state){
+                case BluetoothAdapter.STATE_OFF:
+                    showToast("STATE_OFF");
+                    break;
+                case BluetoothAdapter.STATE_ON:
+                    showToast("STATE_ON");
+                    break;
+                case BluetoothAdapter.STATE_TURNING_ON:
+                    showToast("STATE_TURNING_ON");
+                    break;
+                case BluetoothAdapter.STATE_TURNING_OFF:
+                    showToast("STATE_TURNING_OFF");
+                    break;
+                default:
+                    showToast("Unkown STATE");
+                    break;
+            }
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(receiver,filter);
     }
 
     public void isSupportBlueTooth(View view){
@@ -43,4 +76,15 @@ public class MainActivity extends AppCompatActivity {
         }
         mToast.show();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( resultCode == RESULT_OK){
+            showToast("打开成功");
+        }else {
+            showToast("打开失败");
+        }
+    }
+
 }
